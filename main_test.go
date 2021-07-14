6f827a826fd119a29aecda7801e12e098e13045a
Mark Caudill <mark@mrkc.me>
2021-07-14 09:58:40 -0400

    Fill out tests for GetLockID()
    Signed-off-by: Mark Caudill <mark@mrkc.me>

--- main_test.go
+++ main_test.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"crypto/sha256"
+	"database/sql"
 	"hash"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/markcaudill/terraform-http-backend/terraform"
 )
 
 func TestGetStateID(t *testing.T) {
@@ -49,15 +54,110 @@ func TestHttpRepsonse(t *testing.T) {
 }
 
 func TestGetLockID(t *testing.T) {
-	var (
-		testInput      = `{"ID":"ebd786a4-fb72-f8ad-9705-d6ba623552c2","Operation":"OperationTypeApply","Info":"","Who":"mark@fry","Version":"1.0.1","Created":"2021-07-12T17:29:27.616435429Z","Path":""}`
-		expectedOutput = `ebd786a4-fb72-f8ad-9705-d6ba623552c2`
-	)
-	actualOutput, err := GetLockID([]byte(testInput))
-	if err != nil {
-		t.Fatalf("Error %v", err)
+	tt := []struct {
+		name            string
+		input           string
+		outputValidator func(string)
+		errValidator    func(error)
+	}{
+		{
+			"good input",
+			`{"ID":"ebd786a4-fb72-f8ad-9705-d6ba623552c2","Operation":"OperationTypeApply","Info":"","Who":"mark@fry","Version":"1.0.1","Created":"2021-07-12T17:29:27.616435429Z","Path":""}`,
+			func(str string) {
+				if str != `ebd786a4-fb72-f8ad-9705-d6ba623552c2` {
+					t.Errorf("got %v, expected %s", str, `ebd786a4-fb72-f8ad-9705-d6ba623552c2`)
+				}
+			},
+			func(err error) {
+				if err != nil {
+					t.Errorf("got %v, expected nil", err)
+				}
+			},
+		},
+		{
+			"bad json",
+			`{"ID:"ebd786a4-fb72-f8ad-9705-d6ba623552c2","Operation":"OperationTypeApply","Info":"","Who":"mark@fry","Version":"1.0.1","Created":"2021-07-12T17:29:27.616435429Z","Path":""}`,
+			func(str string) {
+				if str != `` {
+					t.Errorf("got %v, expected %s", str, ``)
+				}
+			},
+			func(err error) {
+				if !strings.Contains(err.Error(), `invalid character`) {
+					t.Errorf(`got %v, expected "invalid character ..."`, err)
+				}
+			},
+		},
+		{
+			"no ID",
+			`{"Operation":"OperationTypeApply","Info":"","Who":"mark@fry","Version":"1.0.1","Created":"2021-07-12T17:29:27.616435429Z","Path":""}`,
+			func(str string) {
+				if str != `` {
+					t.Errorf("got %v, expected %s", str, ``)
+				}
+			},
+			func(err error) {
+				if !strings.Contains(err.Error(), `Error parsing "ID" from`) {
+					t.Errorf(`got %v, expected "Error parsing "ID" from"`, err)
+				}
+			},
+		},
 	}
-	if actualOutput != expectedOutput {
-		t.Fatalf("Got %s, expected %s", actualOutput, expectedOutput)
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			actualOutput, err := GetLockID([]byte(tc.input))
+			tc.errValidator(err)
+			tc.outputValidator(actualOutput)
+		})
+	}
+}
+
+func TestOpenDB(t *testing.T) {
+	tt := []struct {
+		name         string
+		path         string
+		schema       *terraform.StateSchema
+		dbValidator  func(*sql.DB)
+		errValidator func(error)
+		cleanup      func()
+	}{
+		{"good file path", "test.db",
+			terraform.DefaultStateSchema(),
+			func(db *sql.DB) {
+				if db == nil {
+					t.Fatal("got nil, expected !nil")
+				}
+			},
+			func(err error) {
+				if err != nil {
+					t.Fatalf("got %v, expected nil", err)
+				}
+			},
+			func() {
+				os.Remove("test.db")
+			},
+		},
+		{"bad file path", "/:$#.db",
+			terraform.DefaultStateSchema(),
+			func(db *sql.DB) {
+				if db != nil {
+					t.Fatalf("got %v, expected nil", db)
+				}
+			},
+			func(err error) {
+				if err == nil {
+					t.Fatal("got nil, expected !nil")
+				}
+			},
+			func() {},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			defer tc.cleanup()
+			db, err := openDB(tc.path, tc.schema)
+			tc.dbValidator(db)
+			tc.errValidator(err)
+		})
 	}
 }
